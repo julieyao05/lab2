bda002d3ace454a1dd32c6f0e5350f6d557fe31e
mika.2638@gmail.com <appleapple@Julies-MacBook-Air-4.local>
2016-11-22 12:15:14 -0800

    modified

--- app.js
+++ app.js
@@ -60,7 +60,8 @@ if ('development' == app.get('env')) {
 
 // Add routes here
 app.get('/', index.view);
-app.get('/homepage2', index.view2);
+
+app.get('/user2', user.view);
 app.get('/signup', signup.view);
 app.get('/add', add.view);
 app.get('/info', info.view);
